80019d9a8243d8c58d458e7472a766f70783245a
Tom Ward <tom@popdog.net>
2019-10-21 12:35:48 +0100

    DATABASE_URI or profile

--- src/config.clj
+++ src/config.clj
@@ -3,6 +3,7 @@
    [aero.core :refer (read-config)]))
 
 (def config
-  (read-config (clojure.java.io/resource "config.edn")))
+  (read-config (clojure.java.io/resource "config.edn") {:profile :dev}))
+
 
 (prn config)
