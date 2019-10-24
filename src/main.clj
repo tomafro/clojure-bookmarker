8264044790fa11791599163adc35b9d2f7d77410
Tom Ward <tom@popdog.net>
2019-10-24 15:58:01 +0100

    Better errors? And an x-request-id

--- src/main.clj
+++ src/main.clj
@@ -3,33 +3,40 @@
    [aero.core :refer (read-config)]
    [io.pedestal.http :as http]
    [io.pedestal.http.route :as route]
+   [io.pedestal.interceptor.helpers :as interceptor]
    [io.pedestal.test :as test]
    [routes]))
 
 (def server (atom nil))
 
+(defn uuid [] (.toString (java.util.UUID/randomUUID)))
+
+(defn add-x-request-id
+  [response]
+  (assoc response :headers (assoc (:headers response) "X-Request-Id" (uuid))))
+
+(defn app-interceptors
+  [service]
+  (update-in service [::http/interceptors]
+             #(vec (->> %
+                        (cons (interceptor/on-response add-x-request-id))))))
+
+  ; (update-in service [::http/interceptors]
+  ;            #(vec (->> %
+  ;                       (cons cors/dev-allow-origin)
+  ;                       (cons servlet-interceptor/exception-debug)))))
+
 (def service-map
-  {::http/routes routes/routes
-   ::http/type   :jetty
-   ::http/port   8890})
-
-(defn create-server []
-  (http/create-server service-map))                                                           
-
-(defn start []
-  (swap! server
-         (constantly (http/start (create-server))))                                 
-  nil)
-
-(defn stop []
-  (swap! server http/stop)                                                         
-  nil)
+  (-> {::http/routes routes/routes
+       ::http/type   :jetty
+       ::http/port   8890
+       ::http/join?  false}
+      (http/default-interceptors)
+      (app-interceptors)))
 
 (defn start-dev []
   (reset! server
-          (http/start (http/create-server
-                       (assoc service-map
-                              ::http/join? false)))))
+          (http/start (http/create-server service-map))))
 
 (defn stop-dev []
   (http/stop @server))
