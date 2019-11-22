eea28db2573fdc2a1a9877756257603d1a310b57
Tom Ward <tom@popdog.net>
2019-11-22 16:03:55 +0000

    Include a repl script with pretty exceptions

--- dev/repl.clj
+++ dev/repl.clj
@@ -1,9 +1,24 @@
 (ns repl
-  (:require
-   [clojure.tools.namespace.repl]
-   [io.pedestal.http.route :as route]
-   [http.routes]))
+  (:require [nrepl.server :as nrepl-server]
+            [cider.nrepl :refer [cider-nrepl-handler]]
+            [rebel-readline.main :as rebel]
+            [clojure.java.io :as io]
+            [io.aviso.repl]))
 
-(defn show-routes
-  []
-  (route/print-routes (route/expand-routes http.routes/routes)))
+(defn save-port-file
+  [server]
+  (let [port (:port server)
+        port-file (io/file ".nrepl-port")]
+    (.deleteOnExit port-file)
+    (spit port-file port)))
+
+(defn -main []
+  (let [server (nrepl-server/start-server :handler cider-nrepl-handler)]
+    (println "Started nrepl on port" (:port server))
+    (save-port-file server)
+    (io.aviso.repl/install-pretty-exceptions)
+    (rebel/-main)
+    (System/exit 0)))
+
+(defn throw-error []
+  (throw (Exception. "my exception message")))
